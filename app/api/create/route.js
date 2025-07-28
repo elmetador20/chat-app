b9aa1ed475d2f2a6d56117b76024c0ca34cb9cb6
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-28 14:56:04 +0530

    make changes

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,20 +1,30 @@
-
 import { StreamChat } from "stream-chat";
 import { clerkClient } from "@clerk/nextjs/server";
-const api_key = process.env.USER_KEY;
-const api_secret = process.env.USER_SECRET;
+
+const api_key = process.env.API_KEY;
+const api_secret = process.env.API_SECRET;
 
 export async function POST(request) {
   const serverClient = StreamChat.getInstance(api_key, api_secret);
- const user=await request.json()
-  const token=serverClient.createToken(user.data.id);
-    const client=await clerkClient()
-  console.log("a new user has been created", token)
-  await client.users.updateUserMetadata(user.data.id,{
-    publicMetadata:{
-      token:token
-    }
-  })
+  
+  const body = await request.json();
+  const id = body?.id;
 
-  return Response.json({ message: 'Hello World' })
+  if (!id) {
+    return new Response(JSON.stringify({ error: "User ID is missing" }), {
+      status: 400,
+    });
+  }
+
+  const token = serverClient.createToken(id);
+
+  console.log("A new user has been created:", token);
+
+  await clerkClient.users.updateUserMetadata(id, {
+    publicMetadata: {
+      token: token,
+    },
+  });
+
+  return Response.json({ message: "Token created and stored", token });
 }
