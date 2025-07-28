628966304f38e42ab1a3292edf8e491c4e67e9c2
sharique ahmad <ahmedsharique250@gmail.com>
2025-07-28 14:30:30 +0530

    minor changes

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,9 +1,9 @@
 
 import { StreamChat } from "stream-chat";
 import { clerkClient } from "@clerk/nextjs/server";
-const api_key = "f9aeef7m7muq";
-const api_secret = "2yb6x6wfu57cdj7jjwwhxtu9ht479v5jybkveedccav4kwqe6rzm2kjm4sf98rjx";
-//const user_id="user_30SwoIz844W7tpm6qwWTw0oWqT2";
+const api_key = process.env.USER_KEY;
+const api_secret = process.env.USER_SECRET;
+
 export async function POST(request) {
   const serverClient = StreamChat.getInstance(api_key, api_secret);
  const user=await request.json()
